6d8d159a9134e38a8b40a275f5fbf22888ffc8e7
Soumik <shibmunchandra@gmail.com>
2026-06-06 13:39:46 +0530

    Updated the RetrievalQA Chain to ConversationalQA Chain

--- backend/Query.py
+++ backend/Query.py
@@ -1,8 +1,9 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableParallel,RunnableSequence,RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import AIMessage,HumanMessage
+from langchain_core.prompts import MessagesPlaceholder,ChatPromptTemplate
+from langchain_classic.chains import (create_history_aware_retriever,create_retrieval_chain)
+from langchain_classic.chains.combine_documents import (create_stuff_documents_chain)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,37 +21,57 @@ retriever=vector_store.as_retriever(
     search_kwargs={"k":2}
 )
 
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-def format_doc(retrieved_docs):
-    context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
-    return context_text
-
-
-parallel_chain=RunnableParallel({
-        'context':retriever | format_doc,
-        'question':RunnablePassthrough()
-    })
-    
-llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-
-parser=StrOutputParser()
-
-prompt=PromptTemplate(
-    template="""
-    You are an experienced Legal Advisor
-    Answer only from the provided transcript context.
-    If the context is insufficient just tell I don't know.
-    
-    {context}
-    Question:{question}
-    """,
-    input_variables=['context','question']
+contextualize_q_system_prompt=(
+    """
+    Given a chat history and a latest user quetsion
+    which might reference context in the chat history,
+    formulate a standalone question which can be understood
+    without the chat history.DO NOT answer the question,just
+    reformulate it if needed and otherwise return it as it is.
+    """
 )
 
-main_chain = parallel_chain | prompt | llm | parser
+contextualize_q_prompt=ChatPromptTemplate.from_messages(
+    [("system",contextualize_q_system_prompt),
+    MessagesPlaceholder("chat_history"),
+    ("human","{input}"),
+    ]
+)
 
-def query_answer(question:str):
-    return main_chain.invoke(question)
+history_aware_retriever=create_history_aware_retriever(
+    model,retriever,contextualize_q_prompt
+)
 
-print(query_answer("What is Early Termination?"))
-    
+qa_system_prompt=(
+    """
+    You are a Legal Advisor assistant for question-answering the
+    questions asked by the user.Use the following pieces of retrieved
+    context to answer the question.If you don't know the answer, just
+    say I don't know.Use five sentences maximum to answer the question
+    in concise.
+    \n\n
+    {context}
+    """
+)
+
+qa_prompt=ChatPromptTemplate.from_messages(
+    [("system",qa_system_prompt),
+    MessagesPlaceholder("chat_history"),
+    ("human","{input}"),
+    ]
+)
+
+question_answer_chain=create_stuff_documents_chain(model,qa_prompt)
+rag_chain=create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+chat_history=[]
+while(True):
+    query=input("\nUser:")
+    if(query.lower()=="exit"):
+        break
+    ans=rag_chain.invoke({"input":query,"chat_history":chat_history})
+    print(f"AI: {ans["answer"]}")
+    chat_history.append(HumanMessage(content=query))
+    chat_history.append(AIMessage(content=ans["answer"]))
